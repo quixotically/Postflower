bb3885206ee1110c4c740945309b01c3aeb87306
felicia <quixoticf@gmail.com>
2015-07-20 17:42:14 -0400

    Fill in users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,24 @@
 class Api::UsersController < Api::ApiController
-  
+  wrap_parameters false
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:fname, :lname, :email, :password)
+    end
 end
