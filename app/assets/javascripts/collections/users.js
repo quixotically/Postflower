fe184be732db35a3b388b5040c7cb22e4ff648a4
felicia <quixoticf@gmail.com>
2015-07-20 20:36:01 -0400

    Write backbone user model and users collection

--- app/assets/javascripts/collections/users.js
+++ app/assets/javascripts/collections/users.js
@@ -1,5 +1,17 @@
 Postflower.Collections.Users = Backbone.Collection.extend({
+  url: "/api/users",
+  model: Postflower.Models.User,
 
-  model: Postflower.Models.User
+  getAndFetch: function (id) {
+    var user = this.get(id),
+        users = this;
+    if (user) {
+      user.fetch();
+    } else {
+      user = new this.model({ id: id });
+      user.fetch({ success: function () { users.add(user); }});
+    }
 
+    return user;
+  }
 });
